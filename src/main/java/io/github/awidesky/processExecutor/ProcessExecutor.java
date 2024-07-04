fc424779fed831aab943b25bee5c951047c58596
Eugene Hong <awidesky@naver.com>
2024-07-04 18:44:16 +0900

    add more methods for ProcessIO

--- src/main/java/io/github/awidesky/processExecutor/ProcessExecutor.java
+++ src/main/java/io/github/awidesky/processExecutor/ProcessExecutor.java
@@ -1,22 +1,15 @@
 package io.github.awidesky.processExecutor;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 import io.github.awidesky.guiUtil.Logger;
-import io.github.awidesky.guiUtil.SwingDialogs;
 
 
 /**
@@ -24,7 +17,6 @@ import io.github.awidesky.guiUtil.SwingDialogs;
  * */
 public class ProcessExecutor {
 	
-	private static final Charset NATIVECHARSET = Charset.forName(System.getProperty("native.encoding"));;
 	private static ExecutorService executorService = null;
 	
 	public static void setThreadPool(ExecutorService threadPool) { executorService = threadPool; }
@@ -42,30 +34,9 @@ public class ProcessExecutor {
 		// start process
 		Process p = pb.directory(dir).start();
 		
-		Future<?> f1 = submit(() -> {
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), NATIVECHARSET))) {
-				io.stdout(br);
-			} catch (IOException e) {
-				SwingDialogs.error("Unable to close process input stream!", "Process : " + command.stream().collect(Collectors.joining(" "))
-						+ "\n%e%", e, false);
-			}
-		});
-		Future<?> f2 = submit(() -> {
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream(), NATIVECHARSET))) {
-				io.stderr(br);
-			} catch (IOException e) {
-				SwingDialogs.error("Unable to close process input stream!", "Process : " + command.stream().collect(Collectors.joining(" "))
-						+ "\n%e%", e, false);
-			}
-		});
-		Future<?> f3 = submit(() -> {
-			try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(p.getOutputStream(), NATIVECHARSET))) {
-				io.stdin(bw);
-			} catch (IOException e) {
-				SwingDialogs.error("Unable to close process input stream!", "Process : " + command.stream().collect(Collectors.joining(" "))
-						+ "\n%e%", e, false);
-			}
-		});
+		Future<?> f1 = submit(() -> io.stdout(p.getInputStream()));
+		Future<?> f2 = submit(() -> io.stderr(p.getErrorStream()));
+		Future<?> f3 = submit(() -> io.stdin(p.getOutputStream()));
 		
 		return new ProcessHandle(p, f1, f2, f3); 
 	}
--- src/main/java/io/github/awidesky/processExecutor/ProcessIO.java
+++ src/main/java/io/github/awidesky/processExecutor/ProcessIO.java
@@ -2,51 +2,158 @@ package io.github.awidesky.processExecutor;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import io.github.awidesky.guiUtil.Logger;
+import io.github.awidesky.guiUtil.SwingDialogs;
 
 public class ProcessIO {
+	private static final Charset NATIVECHARSET = Charset.forName(System.getProperty("native.encoding"));
 	
-	private Consumer<BufferedReader> stdout = null;
-	private Consumer<BufferedReader> stderr = null;
-	private Consumer<BufferedWriter> stdin = null;
+	private Consumer<InputStream> stdout = null;
+	private Consumer<InputStream> stderr = null;
+	private Consumer<OutputStream> stdin = null;
 	
+	public ProcessIO() {}
 	public ProcessIO(Logger logger) {
-		stdout = br -> br.lines().forEach(logger::log);
-		stderr = br -> br.lines().forEach(logger::log);
+		setStdout(logger);
+		setStderr(logger);
 	}
 	public ProcessIO(Logger out, Logger err) {
-		stdout = br -> br.lines().forEach(out::log);
-		stderr = br -> br.lines().forEach(err::log);
+		setStdout(out);
+		setStderr(err);
 	}
+	public ProcessIO(Logger logger, Consumer<BufferedWriter> stdin) {
+		this(logger);
+		setBufferedStdin(stdin);
+	}
+	
 	public ProcessIO(Consumer<BufferedReader> stdout, Consumer<BufferedReader> stderr) {
-		this.stdout = stdout;
-		this.stderr = stderr;
+		setBufferedStdout(stdout);
+		setBufferedStderr(stderr);
 	}
 	public ProcessIO(Consumer<BufferedReader> stdout, Consumer<BufferedReader> stderr, Consumer<BufferedWriter> stdin) {
-		this.stdout = stdout;
-		this.stderr = stderr;
-		this.stdin = stdin;
+		this(stdout, stderr);
+		setBufferedStdin(stdin);
 	}
 	
-	public void stdout(BufferedReader br) {
-		stdout.accept(br);
+	public ProcessIO stdout(InputStream is) {
+		stdout.accept(is);
+		return this;
 	}
-	public void stderr(BufferedReader br) {
-		stderr.accept(br);
+	public ProcessIO stderr(InputStream is) {
+		stderr.accept(is);
+		return this;
 	}
-	public void stdin(BufferedWriter bw) {
-		if(stdin != null) stdin.accept(bw);
+	public ProcessIO stdin(OutputStream os) {
+		if(stdin != null) stdin.accept(os);
+		return this;
 	}
 	
-	public void setStdout(Consumer<BufferedReader> stdout) {
+	public ProcessIO setStdout(Consumer<InputStream> stdout) {
 		this.stdout = stdout;
+		return this;
 	}
-	public void setStderr(Consumer<BufferedReader> stderr) {
+	public ProcessIO setStderr(Consumer<InputStream> stderr) {
 		this.stderr = stderr;
+		return this;
 	}
-	public void setStdin(Consumer<BufferedWriter> stdin) {
+	public ProcessIO setStdin(Consumer<OutputStream> stdin) {
 		this.stdin = stdin;
+		return this;
+	}
+	/**
+	 * If an Exception is throw, shows the error via swingDialogs.
+	 */
+	public ProcessIO setBufferedStdout(Consumer<BufferedReader> stdout) {
+		this.stdout = is -> {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is, NATIVECHARSET))) {
+				stdout.accept(br);
+			} catch (IOException e) {
+				SwingDialogs.error("Unable to close process input stream!", "%e%", e, false);
+			}
+		};
+		return this;
+	}
+	/**
+	 * If an Exception is throw, shows the error via swingDialogs.
+	 */
+	public ProcessIO setBufferedStderr(Consumer<BufferedReader> stderr) {
+		this.stderr = is -> {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is, NATIVECHARSET))) {
+				stderr.accept(br);
+			} catch (IOException e) {
+				SwingDialogs.error("Unable to close process input stream!", "%e%", e, false);
+			}
+		};
+		return this;
+	}
+	/**
+	 * If an Exception is throw, shows the error via swingDialogs.
+	 */
+	public ProcessIO setBufferedStdin(Consumer<BufferedWriter> stdin) {
+		this.stdin = is -> {
+			try (BufferedWriter br = new BufferedWriter(new OutputStreamWriter(is, NATIVECHARSET))) {
+				stdin.accept(br);
+			} catch (IOException e) {
+				SwingDialogs.error("Unable to close process input stream!", "%e%", e, false);
+			}
+		};
+		return this;
+	}
+	/**
+	 * Does not show or throw any error. 
+	 */
+	public ProcessIO setPrintStdout(Consumer<Scanner> stdout) {
+		this.stdout = is -> {
+			stdout.accept(new Scanner(new InputStreamReader(is, NATIVECHARSET)));
+		};
+		return this;
+	}
+	/**
+	 * Does not show or throw any error. 
+	 */
+	public ProcessIO setPrintStderr(Consumer<Scanner> stderr) {
+		this.stderr = is -> {
+			stderr.accept(new Scanner(new InputStreamReader(is, NATIVECHARSET)));
+		};
+		return this;
+	}
+	/**
+	 * Does not show or throw any error. 
+	 */
+	public ProcessIO setPrintStdin(Consumer<PrintWriter> stdin) {
+		this.stdin = is -> {
+			stdin.accept(new PrintWriter(new OutputStreamWriter(is, NATIVECHARSET)));
+		};
+		return this;
+	}
+	public ProcessIO setStdout(Logger logger) {
+		setBufferedStdout(br -> br.lines().forEach(logger::log));
+		return this;
+	}
+	public ProcessIO setStderr(Logger logger) {
+		setBufferedStderr(br -> br.lines().forEach(logger::log));
+		return this;
+	}
+	/**
+	 * input stream will terminate when null is supplied. 
+	 */
+	public ProcessIO setStdin(Supplier<String> s) {
+		setPrintStdin(pw -> {
+			Stream.generate(s::get).takeWhile(Objects::nonNull).forEach(pw::println);
+		});
+		return this;
 	}
 }
